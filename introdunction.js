22cc628f0dcca56adbfc35e30743e2329c06b7ef
misprachi023 <prachimishra23123@gmail.com>
2023-09-07 14:24:29 +0530

    added introduction to js code

---
+++ introdunction.js
@@ -0,0 +1,55 @@
+console.log("Masai School");
+console.log("A Tranformation in Education");
+
+
+
+var name="Prachi Mishra";
+console.log("Name -" ,name);
+var Father_name="Manbharan Mishra";
+console.log("Father name -", Father_name);
+var Mother_name="Mamta Mishra";
+console.log("Mother name -", Mother_name);
+
+
+var name="Prachi Mishra";
+var age=21;
+// console.log(name, /n age)
+console.log(name,age);
+console.log(typeof(name), typeof(age));
+          
+
+
+console.log(`                🆁 🅴 🆂 🆄 🅻 🆃`)
+console.log(" ** ** ** ** ** ** ** ** ** ** ** ** ** ** ")
+console.log("*                                         *")
+console.log(`             🆁 🅴 🅿 🅾 🆁 🆃  🅲 🅰 🆁 🅳         `);
+console.log("*                                         *")
+var name="Prachi Mishra";
+var school= "Rose Mary Hr.Sec. School";
+var grade= "A";
+var section= "B";
+var roll= 67;
+var Physics= 87;
+var Chemistry=84;
+var Maths= 88;
+console.log("      ~ Name-", name);
+console.log("*                                         *")
+console.log("      ~ School-", school);
+console.log("*                                         *")
+console.log("      ~ Grade-", grade);
+console.log("*                                         *")
+console.log("      ~ Section-", section);
+console.log("*                                         *")
+console.log("      ~ Roll no-", roll);
+console.log("*                                         *")
+console.log(`        🅼 🅰 🆁 🅺 🆂  🅾 🅱 🆃 🅰 🅸 🅽 🅴 🅳`);
+console.log("*                                         *")
+console.log("        ~ Physics-",Physics);
+console.log("*                                         *")
+console.log("        ~ Chemistry-",Chemistry);
+console.log("*                                         *")
+console.log("        ~ Maths-",Maths);
+console.log("*                                         *")
+console.log(" ** ** ** ** ** ** ** ** ** ** ** ** ** ** ")
+
+
